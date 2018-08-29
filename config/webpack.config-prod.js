4442585f5e3a24d86ca0f3af71d6fc7b86c4d837
ric <ric@snapzio.com>
2018-08-30 07:20:49 +0800

    more config for prod

--- config/webpack.config-prod.js
+++ config/webpack.config-prod.js
@@ -3,10 +3,22 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devConfig = require('./webpack.config');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+const VENDOR_LIBS = [
+  'react',
+  'react-dom',
+  'prop-types',
+];
+
 const config = env => {
   const prodConfig = { ...devConfig(env) };
   // remove devServer
   delete prodConfig.devServer;
+
+  // add vendor entry
+  prodConfig.entry.vendor = VENDOR_LIBS;
+
+  // add output chunk
+  prodConfig.output.chunkFilename = '[name].bundle.js';
 
   // remove 'style-loader' and replace with MiniCssExtractPlugin
   prodConfig.module.rules[1] = {
@@ -38,7 +50,21 @@ const config = env => {
         parallel: 10,
       }),
     ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: 'vendor',
+          enforce: true
+        },
+      }
+    },
   };
+
+  // performance monitor settings
+  prodConfig.performance = { hints: false };
 
   return prodConfig;
 };
