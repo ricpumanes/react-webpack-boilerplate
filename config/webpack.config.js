d812e00d96a883da9ad812d8cd0402aee5a730c3
ric <ric@snapzio.com>
2018-08-29 13:59:52 +0800

    spacing

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,13 +29,13 @@ const webpackConfig = env => {
           },
         },
         {
-          test: /\.scss$/,
+          test: /\.(scss|sass)$/,
           use: [
-              "style-loader", // creates style nodes from JS strings
-              "css-loader", // translates CSS into CommonJS
-              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
           ],
-        }
+        },
       ],
     },
     resolve: {
