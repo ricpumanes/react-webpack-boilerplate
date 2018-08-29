45335c489b30004fda1c9d35b8bf2cee80ddb598
ric <ric@snapzio.com>
2018-08-29 13:58:09 +0800

    handle sass

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -27,6 +27,14 @@ const webpackConfig = env => {
           use: {
             loader: 'babel-loader',
           },
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              "style-loader", // creates style nodes from JS strings
+              "css-loader", // translates CSS into CommonJS
+              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          ],
         }
       ],
     },
