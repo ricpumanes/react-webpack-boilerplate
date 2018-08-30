9dddac2c38a26476a9910c38e33532cbf0411dcd
ricpumanes <ricpumanes@gmail.com>
2018-08-30 17:18:19 +0800

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const env = process.env.NODE_ENV;
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const NotFound = () => <h1>Not Found</h1>;
 
 class App extends Component {
   static propTypes = {
@@ -9,14 +14,21 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        app!!
-        Lorem ipsum dolor sit amet.
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/*" component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
+
 let parent = App;
+
 if (env !== 'prod'){
     parent = require('react-hot-loader').hot(module)(App);
 }
