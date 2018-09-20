96e44089ef285f874740be5259f95ffe469b317a
ricpumanes <ricpumanes@gmail.com>
2018-09-20 10:05:25 +0800

    convert App to functional component, update loading text

--- src/App.js
+++ src/App.js
@@ -8,23 +8,18 @@ const Home = () => <h1>Home</h1>;
 const About = () => <h1>About</h1>;
 const NotFound = () => <h1>Not Found</h1>;
 
-class App extends Component {
-  static propTypes = {
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/*" component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/*" component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 let parent = App;
