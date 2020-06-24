4a02dbb7c1213639fd6ba82a57c910dc59d42e54
ric <ric@snapzio.com>
2020-06-24 11:08:44 +0800

    setup styled

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import "../public/assets/styles/sass/_index.scss";
 
 import App from "./App";
 import store from "./logic/store";
+import { init as initFeathersClient } from "./feathersClient";
+
+initFeathersClient();
 
 render(
   <Provider store={store}>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
 
 const Home = (props) => {
   return (
-    <div>
-      <div className="h-screen bg-gray-500">home</div>
+    <div className="h-screen">
+      <div className="bg-gray-500">home</div>
+      <Button>CLick me!</Button>
     </div>
   );
 };
